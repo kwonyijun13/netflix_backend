6489a909287b1a5dfe2f9228ba4a4ad34368db02
kwonyijun13 <89631279+kwonyijun13@users.noreply.github.com>
2025-01-22 13:27:03 +0900

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { authCheck, login, logout, signup } from "../controllers/auth.controller.js";
+import { authCheck, login, logout, register } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/signin", login);
+router.post("/register", register);
+router.post("/login", login);
 router.post("/logout", logout);
 
 router.get("/authCheck", protectRoute, authCheck);
